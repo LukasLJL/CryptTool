21377328a9745df5d86a091f26ee9acc5d5ce067
LukasLJL <lukas@leucali.net>
2021-02-04 10:14:28 +0100

    header for driveSelector.c

---
+++ src/driveSelector.h
@@ -0,0 +1,8 @@
+//
+// Created by Alessandro on 04.02.2021.
+//
+
+#ifndef CRYPT_TOOL_DRIVESELECTOR_H
+#define CRYPT_TOOL_DRIVESELECTOR_H
+
+#endif //CRYPT_TOOL_DRIVESELECTOR_H
--- src/functions.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Alessandro on 04.02.2021.
-//
-
-#ifndef CRYPT_TOOL_FUNCTIONS_H
-#define CRYPT_TOOL_FUNCTIONS_H
-
-#endif //CRYPT_TOOL_FUNCTIONS_H
