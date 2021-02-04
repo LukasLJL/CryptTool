678fdbac6f802fe59673e49de23db6d5b5d6b368
LukasLJL <lukas@leucali.net>
2021-02-04 10:18:06 +0100

    function prototype for driveSelector

--- src/driveSelector.h
+++ src/driveSelector.h
@@ -1,8 +1,1 @@
-//
-// Created by Alessandro on 04.02.2021.
-//
-
-#ifndef CRYPT_TOOL_DRIVESELECTOR_H
-#define CRYPT_TOOL_DRIVESELECTOR_H
-
-#endif //CRYPT_TOOL_DRIVESELECTOR_H
+char *getMasterKeyPath();
