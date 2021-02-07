845e9229db6a3b01b751a404d77d5cb0774ad2b5
grossamos <email@amosgross.com>
2021-02-07 19:56:20 +0100

    added crypt interface

---
+++ cryptlib/cryptlib.h
@@ -0,0 +1,14 @@
+//
+// Created by GROSA5 on 2/7/2021.
+//
+
+#ifndef CRYPT_TOOL_CRYPTLIB_H
+#define CRYPT_TOOL_CRYPTLIB_H
+
+void encrypt_test(char *key, char *path, void *algorithm(char *));
+void decrypt_test(char *key, char *path, void *algorithm(char *));
+
+void encrypt(char *key, char *path);
+void decrypt(char *key, char *path);
+
+#endif //CRYPT_TOOL_CRYPTLIB_H
