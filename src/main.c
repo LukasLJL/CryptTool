7b47562d2dc343b55c17b5a42e2ce23931c9acdc
Ophelion <gameterraro@gmx.de>
2021-02-10 10:52:10 +0100

    main(void) Two cases has been added for de -and encrypt.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "driveSelector.h"
 
 int main(void)
 {
-    printf("AMOS ALA PLAYA");
+    int selectionCryptMode;
+    char writeDriveLetter;
+    char writeFilePath;
+
+    printf("Auswahl von CryptMode:\n");
+    printf("(1) Entschlüsseln");
+    printf("(2) Verschlüsseln");
+
+    scanf("%d", selectionCryptMode);
+
+    switch(selectionCryptMode)
+    {
+        case 1:
+            printf("Eingabe von Laufwerksbuchstaben, wo die Datei liegt:\n");
+            scanf("%s", &writeDriveLetter);
+
+            printf("Eingabe von Pfad, wo die Datei liegt: C:\n");
+            scanf("%s", &writeFilePath);
+
+            getMasterKeyPath(writeDriveLetter, writeFilePath);
+            break;
+
+        case 2:
+            printf("Eingabe von Pfad, wo die Datei liegt:\n");
+            scanf("%s", &writeFilePath);
+
+            printf("Eingabe von Pfad, wo die Datei liegt: C:\n");
+            scanf("%s", &writeFilePath);
+
+            getMasterKeyPath(writeDriveLetter, writeFilePath);
+            break;
+    }
 }
