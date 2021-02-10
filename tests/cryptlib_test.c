cf130d4cc08f3996bea822855a63d1939770e522
LukasLJL <lukas@leucali.net>
2021-02-10 22:13:03 +0100

    fixed test

--- tests/cryptlib_test.c
+++ tests/cryptlib_test.c
@@ -2,13 +2,13 @@
 #include "../cryptlib/cryptlib.h"
 
 #define PATH_TO_KEY "C:\\crypt\\key.dat"
-#define FILE_TO_ENCRYPT "C:\\crypt\\Bigbrain.jpg"
+#define FILE_TO_ENCRYPT "C:\\crypt\\IMG_1402_edit.jpg"
 #define FILE_TO_DECRYPT "C:\\crypt\\newfile.encrypted"
 
 
 int main() {
-    encrypt_test(PATH_TO_KEY, FILE_TO_ENCRYPT, "0");
-    decrypt_test(PATH_TO_KEY, FILE_TO_DECRYPT, "0");
+    encrypt_test(PATH_TO_KEY, FILE_TO_ENCRYPT, (void *(*)(char *)) "0");
+    decrypt_test(PATH_TO_KEY, FILE_TO_DECRYPT, (void *(*)(char *)) "0");
 }
 
 void encrypt_test(char *key, char *path, void *algorithm(char *)){
