8d9e0cf7fc9557903c09fa3c9feef72cff0d0d62
grossamos <email@amosgross.com>
2021-02-10 09:37:57 +0100

    added prototype for internal use

--- cryptlib/cryptlib.h
+++ cryptlib/cryptlib.h
@@ -11,4 +11,8 @@ void decrypt_test(char *key, char *path, void *algorithm(char *));
 void encrypt(char *key, char *path);
 void decrypt(char *key, char *path);
 
+// internal prototypes
+void *toCrypt(char *key, size_t len, void *binData);
+void *fromCrypt(char *key, size_t len, void *binData);
+
 #endif //CRYPT_TOOL_CRYPTLIB_H
