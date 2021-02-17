2516b1d80ea3ee05f4a43744d545816890991f42
LukasLJL <lukas@leucali.net>
2021-02-17 09:25:08 +0100

    excluded getOS

--- cryptlib/cryptlib-io.h
+++ cryptlib/cryptlib-io.h
@@ -1,10 +1,6 @@
-//
-// Created by GROSA5 on 2/5/2021.
-//
-
 #ifndef CRYPT_TOOL_CRYPTLIB_IO_H
 #define CRYPT_TOOL_CRYPTLIB_IO_H
-#include "../src/driveSelector.h"
+#include "cryptlib-osHandling.h"
 
 void catchMemoryAllocationFailure(void *);
 
